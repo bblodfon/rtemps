aaf845a9d0946002e9e865a51c1e1e8e8e5bb993
john <bblodfon@gmail.com>
2020-02-17 21:59:35 +0100

    refactoring + add better doc

--- R/create_temp.R
+++ R/create_temp.R
@@ -8,35 +8,33 @@
 #' @param template which template to use?
 #'
 #' @details
-#' The available templates are: \emph{united_html} (default).
-#' The name of the main \code{.Rmd} file will be \emph{test.Rmd}.
+#' The available templates are: \emph{united_html} (default), \emph{bookdown_lite}.
+#' The name of the main \code{.Rmd} file will be \emph{index.Rmd}.
 #'
 #' @examples
 #' \dontrun{
-#' create_rtemp(dirname = "tmp_dir", format = "united_html")
+#' create_rtemp(dirname = "tmp_dir", template = "united_html")
 #' }
 #'
 #' @export
 create_rtemp = function(dirname = "new-dir", template = "united_html") {
-  templates = c("united_html")
+  # check input
+  templates = c("united_html", "bookdown_lite")
   temp = match.arg(template, templates)
 
+  # create directory
   if (dir.exists(dirname)) {
     stop(paste0("There is already a directory `", dirname,
-      "`. Please supply a different directory name"))
+      "`. Please specify a different directory name"))
   }
-
   dir.create(dirname)
 
-  file.copy(
-    system.file(file.path("rmarkdown", "templates", temp, "skeleton", "skeleton.Rmd"), package = "rtemps"),
-    file.path(dirname)
-  )
-  file.copy(
-    system.file(file.path("rmarkdown", "templates", temp, "skeleton", "style.css"), package = "rtemps"),
-    file.path(dirname)
-  )
+  # copy files from template directory
+  template.dir = system.file(file.path("rmarkdown", "templates", temp, "skeleton"), package = "rtemps")
+  files.to.copy = list.files(template.dir)
+  file.copy(file.path(template.dir, files.to.copy),
+    file.path(dirname), recursive = TRUE)
 
-  file.rename(file.path(dirname, "skeleton.Rmd"), file.path(dirname, "test.Rmd"))
+  file.rename(file.path(dirname, "skeleton.Rmd"), file.path(dirname, "index.Rmd"))
 }
 
